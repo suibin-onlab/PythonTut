50a17d74db973aa35c859eedcdc4fad1c03f4ff0
suibin zhang <suibin@onlab.us>
2016-11-11 21:15:40 -0800

    DFS and BFS in python

---
+++ graph.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+Graph = {
+    'A': set(['B', 'F', 'D']),
+    'B': set(['A', 'C']),
+    'C': set(['B','F', 'G', 'H']),
+    'D': set(['A', 'F', 'E']),
+    'E': set(['D', 'G', 'I']),
+    'F': set(['A', 'C', 'D', 'G']),
+    'G': set(['E', 'F', 'C']),
+    'H': set(['C', 'I']),
+    'I': set(['E', 'H'])
+}
+
+visitedd, stack = [], []
+visitedb =[]
+def dfs(Graph, start = '', end = ''):
+    while stack and len(visitedd) < len(Graph) and end not in visitedd:
+        vertex = stack.pop()
+        visitedd.append(vertex)
+
+        for item in Graph[vertex]:
+            if item not in visitedd:
+                stack.append(item)
+                print "Before recursion, stack is: " + str(stack)
+                print "Before Recursion, visitedd is: " + str(visitedd)
+                return dfs(Graph, start = item, end = end)
+
+    return visitedd
+
+from collections import deque
+q = deque()
+def bfs(Graph, start = '', end = ''):
+
+    while q and len(visitedb) < len(Graph) and end not in visitedb:
+        print "end: {}; visted: {}".format(end, end in visitedb)
+        vertex = q.popleft()
+        visitedb.append(vertex)
+
+        ritem = ''
+        for item in Graph[vertex]:
+            if item not in visitedb and item not in q:
+                q.append(item)
+                ritem = item
+        print "Before iteration, visitedb: " + str(visitedb)
+        print "Before iteration, q: " + str(q)
+        return bfs(Graph, start = ritem, end = end)
+    return visitedb
+
+
+def main():
+    startNode = 'A'
+    endNode = 'I'
+    pth = find_path(Graph, startNode, endNode)
+    print "Path from " + startNode + " to " + endNode  + " is:" + str(pth)
+
+if __name__ == '__main__':
+    s = 'A'
+    e = 'H'
+
+    print "DFS:"
+    stack.append(s)
+    print dfs(Graph, start = s, end = e)
+
+    print "\n"
+    print "BFS:"
+    q.append(s)
+    print bfs(Graph, start = s, end = e)
