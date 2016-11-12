dc60857503d66155cb71c94d7b01b10294109fc0
suibin zhang <suibin@onlab.us>
2016-11-11 21:20:57 -0800

    commented unwanted print statements

--- graph.py
+++ graph.py
@@ -22,8 +22,8 @@ def dfs(Graph, start = '', end = ''):
         for item in Graph[vertex]:
             if item not in visitedd:
                 stack.append(item)
-                print "Before recursion, stack is: " + str(stack)
-                print "Before Recursion, visitedd is: " + str(visitedd)
+                #print "Before recursion, stack is: " + str(stack)
+                #print "Before Recursion, visitedd is: " + str(visitedd)
                 return dfs(Graph, start = item, end = end)
 
     return visitedd
@@ -33,7 +33,7 @@ q = deque()
 def bfs(Graph, start = '', end = ''):
 
     while q and len(visitedb) < len(Graph) and end not in visitedb:
-        print "end: {}; visted: {}".format(end, end in visitedb)
+        #print "end: {}; visted: {}".format(end, end in visitedb)
         vertex = q.popleft()
         visitedb.append(vertex)
 
@@ -42,27 +42,25 @@ def bfs(Graph, start = '', end = ''):
             if item not in visitedb and item not in q:
                 q.append(item)
                 ritem = item
-        print "Before iteration, visitedb: " + str(visitedb)
-        print "Before iteration, q: " + str(q)
+        #print "Before iteration, visitedb: " + str(visitedb)
+        #print "Before iteration, q: " + str(q)
         return bfs(Graph, start = ritem, end = end)
     return visitedb
 
 
 def main():
-    startNode = 'A'
-    endNode = 'I'
-    pth = find_path(Graph, startNode, endNode)
-    print "Path from " + startNode + " to " + endNode  + " is:" + str(pth)
+    s = 'I'
+    e = 'B'
 
-if __name__ == '__main__':
-    s = 'A'
-    e = 'H'
-
-    print "DFS:"
+    print "DFS: Path from {} to {}".format(s, e)
     stack.append(s)
     print dfs(Graph, start = s, end = e)
 
     print "\n"
-    print "BFS:"
+    print "BFS: Path from {} to {}".format(s, e)
     q.append(s)
     print bfs(Graph, start = s, end = e)
+
+
+if __name__ == '__main__':
+    main()
